6f7a2109e1e02b2dd24fc46398e2686794a5b0ab
Ksenia Simonova <ksansi@yandex.ru>
2020-09-21 22:42:27 +0500

    Night commit

---
+++ lab_0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab_0
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст, который хотите вывести:    ");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+            
+            Console.ReadKey();
+        }
+    }
+}
