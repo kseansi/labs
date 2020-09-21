1f7817232c7cf977bff7f44ab91abed84fab85f6
kseansi <70561438+kseansi@users.noreply.github.com>
2020-09-21 20:33:10 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab_0
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст, который хотите вывести:    ");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+        }
+    }
+}
