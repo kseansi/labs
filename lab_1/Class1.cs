81e3eef1e9ce081a7ffb6a796fc236da05308e67
Ksenia Simonova <ksansi@yandex.ru>
2020-09-28 21:56:06 +0500

    initial commit

--- lab_1/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace lab_1
-{
-    public class Class1
-    {
-    }
-}
--- lab_1/Program.cs
+++ lab_1/Program.cs
@@ -1,23 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System;
 
 namespace lab_1
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        //создание объекта - круга, квадрата, прямоугольника
+        public Creating()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+        }
+
+        //перемещение на плоскости на заданный вектор
+        public Moving()
+        {
+
+        }
+
+        //изменение размеров относительно ГЦФ
+        public ChangingScale()
+        {
+
+        }
+
+        //вращение на заданный угол относительно ГЦФ
+        public Rotation()
+        {
+
+        }
+
+        //определение факта наложения двух выбранных фигур
+        public Overlay()
+        {
+            
+        }
+
+        public static void Run()
+        { 
+            /* меню будет здесь!*/
+        }   
+        public static void Main(string[] args)
+        {
+            Run();
         }
     }
 }
