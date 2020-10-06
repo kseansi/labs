893aba80a380656c61fdda20ad6c56b635a6e80c
Ksenia Simonova <ksansi@yandex.ru>
2020-10-06 11:16:12 +0500

    initial commit

--- lab_2/Program.cs
+++ lab_2/Program.cs
@@ -9,4 +9,20 @@ namespace lab_2
             
         }
     }
+    class ComplexNumber
+    {
+
+    }
+    public static ComplexNumber operator + (ComplexNumber comNum, float num)
+    {
+
+    }
+    public static ComplexNumber operator - (ComplexNumber comNum, float num)
+    {
+
+    }
+    public static ComplexNumber operator * (ComplexNumber comNum, float num)
+    {
+
+    }
 }
