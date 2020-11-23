64e91a0aab1138145c018a0df3bb8bca9f57903a
Ksenia Simonova <ksansi@yandex.ru>
2020-11-23 20:17:25 +0500

    доделала там короче всё кроме вывода

--- lab_2/Program.cs
+++ lab_2/Program.cs
@@ -17,23 +17,49 @@ namespace lab_2
             int tmp1 = int.Parse(Console.ReadLine());
             if (tmp1 == 1)
             {//действия с одним комплексным числом (умножение на вещественное число)
+                Console.WriteLine("Введите real часть числа");
+                int r = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите imaginary часть числа");
+                int i = int.Parse(Console.ReadLine());
+                ComplexNumber complex = new ComplexNumber 
+                {
+                real = r,
+                imaginary = i
+                };
                 Console.WriteLine("Введите множитель");
                 int factor = int.Parse(Console.ReadLine());
+                complex *= factor;
             }
             else
             {//действия с двумя комплаксными числами (сложение и вычитание)
                 Console.WriteLine("1. Сложение");
                 Console.WriteLine("2. Вычитание");
                 int tmp2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите real часть 1 числа");
+                int r1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите imaginary часть 1 числа");
+                int i1 = int.Parse(Console.ReadLine());
+                ComplexNumber complex1 = new ComplexNumber 
+                {
+                real = r1,
+                imaginary = i1
+                };
+                Console.WriteLine("Введите real часть 2 числа");
+                int r2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите imaginary часть 2 числа");
+                int i2 = int.Parse(Console.ReadLine());
+                ComplexNumber complex2 = new ComplexNumber 
+                {
+                real = r2,
+                imaginary = i2
+                };                
                 if (tmp2 == 1)
                 {//сложение
-                    Console.WriteLine("2. Операции с двумя к.ч.");
-                    Console.WriteLine("1. Сложение");
+                    complex1 += complex2;
                 }
                 else
                 {//вычитание
-                    Console.WriteLine("2. Операции с двумя к.ч.");
-                    Console.WriteLine("2. Вычитание");
+                    complex1-=complex2;
                 }
             }
         }
@@ -42,6 +68,7 @@ namespace lab_2
     {
         public int real;
         public int imaginary;
+        //public ComplexNumber(int r, int i){real = r; imaginary = i;}
         public static ComplexNumber operator + (ComplexNumber comNum1, ComplexNumber comNum2)
         {
             ComplexNumber CN = new ComplexNumber
